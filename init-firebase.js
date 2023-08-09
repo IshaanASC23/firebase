e76bb4ef58dc875cef036f5d9f43232290f3a208
Ishaan Dhital <dhitaliSA19@gmail.com>
2023-08-09 15:36:36 -0400

    finished project

--- init-firebase.js
+++ init-firebase.js
@@ -3,7 +3,12 @@
  * provided for you by firebase after creating your project!
 */
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyC2eBUXpGy0d23JgJTgoRteISn2KEboG_4",
+    authDomain: "firemessages-94443.firebaseapp.com",
+    projectId: "firemessages-94443",
+    storageBucket: "firemessages-94443.appspot.com",
+    messagingSenderId: "6687548523",
+    appId: "1:6687548523:web:f113a0dca180bd020a3d2e"
 };
 
 // initialize the firebase app with this built-in function
